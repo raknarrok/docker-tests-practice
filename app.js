76973ff090da2da2c744475c4a645220ed1aa23e
Raknarrok <raknarrok@hotmail.com>
2025-11-03 14:09:42 -0600

    Initialize project with node-cron example
    Add initial project files including .gitignore, package.json, and a simple app.js that uses node-cron to log a message every 5 seconds. Sets up basic project structure for Docker Compose and PostgreSQL integration.

---
+++ app.js
@@ -0,0 +1,10 @@
+const cron = require('node-cron')
+
+let times = 0;
+
+cron.schedule('1-59/5 * * * * *', () => {
+  times += 1
+  console.log('Tick cada 5 segs ', times)
+});
+
+console.log("Start")
